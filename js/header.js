e3bb86481e95e29385001bd9dbd0b9d25f85c66d
Hotdoy <rouleau.vincent@gmail.com>
2020-06-04 15:28:24 -0400

    basic header behaviour and style

--- js/header.js
+++ js/header.js
@@ -1,37 +1,13 @@
 let	b = document.body;
-let onTriggers = document.getElementsByClassName("header-panel__on");
-let offTriggers = document.getElementsByClassName("header-panel__off");
+let navToggles = document.getElementsByClassName("nav-toggle");
 let lastOffset = 0;
 
-for (let onTrigger of onTriggers) {
-	onTrigger.addEventListener("click", function(){
-		b.classList.add("header-panel--on");
+for (let navToggle of navToggles) {
+	navToggle.addEventListener("click", function(){
+		if (b.classList.contains('nav--on')) {
+			b.classList.remove('nav--on');
+		} else {
+			b.classList.add('nav--on')
+		}
 	})
 }
-
-for (let offTrigger of offTriggers) {
-	offTrigger.addEventListener("click", function(){
-		b.classList.remove("header-panel--on");
-	})
-}
-
-window.addEventListener('resize', function(){
-	if (window.innerWidth > 735 && b.classList.contains("header-panel--on")) {
-		b.classList.remove("header-panel--on");
-	}
-});
-
-window.addEventListener("scroll", function(){
-
-   	let currentOffset = window.pageYOffset || document.documentElement.scrollTop;
-
-   	if (currentOffset < lastOffset && window.pageYOffset > 200){
-		b.classList.add("header-onscroll--on"); 
-   	} else if (currentOffset > (lastOffset + 10) || window.pageYOffset < 200) {
-    	b.classList.remove("header-onscroll--on");
-   	}
-
-   	lastOffset = currentOffset;
-}, false);
-
-
